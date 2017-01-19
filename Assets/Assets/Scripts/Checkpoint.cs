739230dc44ffb1794c0b6df8df3b717dcf7ec73f
JeffrinDumas <jeffrin.e.d@live.nl>
2017-01-19 11:23:40 +0100

    daily

--- Assets/Assets/Scripts/Checkpoint.cs
+++ Assets/Assets/Scripts/Checkpoint.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour 
 {
-	public List<GameObject> checkpoints = new List<GameObject>();
+	public List<Transform> checkpointPositions = new List<Transform>();
 	private int currentCheckpoint = 0;
 	public GameObject checkPrefab;
 	private Rigidbody rbody;
 
 	void Start () {
-        checkpoints.Add (new GameObject());
-		checkpoints.Add (new GameObject());
-		checkpoints.Add (new GameObject());
-		checkpoints.Add (new GameObject());
-		checkpoints.Add (new GameObject());
+        //     checkpointPositions.Add( new Vector3 (0, 20, 25));
+        //     checkpointPositions.Add(new Vector3(100, 25, 25));
+        //     checkpointPositions.Add(new Vector3(150, 30, 25));
+        //    checkpointPositions.Add(new Vector3(200, 35, 25));
+        //      checkpointPositions.Add(new Vector3(250, 40, 25));
 
-		checkpoints[0].transform.position = new Vector3 (0, 20, 25);
-		checkpoints[1].transform.position = new Vector3 (10, 25, 25);
-		checkpoints[2].transform.position = new Vector3 (15, 30, 25);
-		checkpoints[3].transform.position = new Vector3 (20, 35, 25);
-		checkpoints[4].transform.position = new Vector3 (25, 40, 25);
-	
-		
-		rbody = GetComponent<Rigidbody>();
+        Instantiate(checkPrefab, checkpointPositions[currentCheckpoint].position, checkpointPositions[currentCheckpoint].rotation);
+
+        rbody = GetComponent<Rigidbody>();
 
 		//GameVariables.checkpoint = new Vector3 (0, 4.3f, 0);
 	}
@@ -33,7 +28,9 @@ public class Checkpoint : MonoBehaviour
 	void OnTriggerEnter(Collider col) {
        
 		if(col.gameObject.tag == "Checkpoint"){
-			currentCheckpoint++;
+            
+            GameObject.Destroy(col.gameObject);
+            currentCheckpoint++;
 			showCheckpoint ();
 		//transform.position = GameVariables.checkpoint;
 		}
@@ -42,8 +39,8 @@ public class Checkpoint : MonoBehaviour
 
 	}
 	public void showCheckpoint(){
-		GameObject.Instantiate(checkPrefab, checkpoints[currentCheckpoint].transform);
-	}
+        Instantiate(checkPrefab, checkpointPositions[currentCheckpoint].position, checkpointPositions[currentCheckpoint].rotation);
+    }
 
     void Update()
     {
