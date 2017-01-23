7e975af271a1f672973f71476824bf1c26a530b2
nickvaningen <nickvaningen@hotmail.com>
2017-01-23 11:11:08 +0100

    enemy

--- Assets/Assets/Scripts/EnemyMovement.cs
+++ Assets/Assets/Scripts/EnemyMovement.cs
@@ -12,6 +12,7 @@ namespace Assets.Assets.Scripts
         public float AttackTime;
         public float CoolDown;
         private readonly Transform _myTransform;
+        public Transform player;
 
         public EnemyMovement(Transform myTransform)
         {
@@ -31,7 +32,12 @@ namespace Assets.Assets.Scripts
         private void Update()
         {
             Seek();
-
+            
+          
+            if (player != null)
+            {
+                transform.LookAt(player);
+            }
             Debug.DrawLine(Target.position, _myTransform.position, Color.red);
             var dist = Vector3.Distance(Target.position, transform.position);
             if (dist < 3)
@@ -63,5 +69,10 @@ namespace Assets.Assets.Scripts
 
             GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity + steeringForce / Mass;
         }
+
+        void OnTriggerEnter(Collider other) {
+        Destroy(this.gameObject);
+    }
+
     }
 }
