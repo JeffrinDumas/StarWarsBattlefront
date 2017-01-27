4ac20df91a62b3db13dd9fefebf7dd10b232a62e
nickvaningen <nickvaningen@hotmail.com>
2017-01-27 13:44:05 +0100

    enemy

---
+++ Assets/Assets/Scripts/tiefighterbolt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tiefighterbolt : MonoBehaviour
+{
+    public GameObject tiefighterlaserbolt;
+    private Transform myTransform;
+    public float force;
+
+	void Start ()
+	{
+	    setinitialReferences();
+
+	}
+	
+
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.E))
+	    {
+	        spawnLazerBolt();
+	    }
+	}
+
+    void setinitialReferences()
+    {
+        myTransform = transform;
+    }
+
+    void spawnLazerBolt()
+    {
+        GameObject go = (GameObject)Instantiate(tiefighterlaserbolt, myTransform.TransformPoint(0, 0, 0.5f), myTransform.rotation);
+        go.GetComponent<Rigidbody>().AddForce(myTransform.forward * force, ForceMode.Impulse);
+        Destroy(go, 3);
+    }
+}
