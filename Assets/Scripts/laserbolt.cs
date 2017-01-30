8b1904d61893410c147202b69c0ff0872c646648
nickvaningen <nickvaningen@hotmail.com>
2017-01-30 10:58:03 +0100

    enemy

---
+++ Assets/Scripts/laserbolt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserbolt : MonoBehaviour {
+
+    [SerializeField]
+    private spawn spawner;
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "laserbolt")
+        {
+            GameObject.Destroy(col.gameObject);
+        }
+    }
+}
+
